8e1e1d599dc6f6777d4e01f9bbe34124b8c4bf3a
irwantoadi <irwantoadi926@gmail.com>
2023-01-14 20:55:48 +0700

    project prototype media pembelajaran islami dalibin

---
+++ app/src/main/java/com/irwantoadi/dalibin/BinbookEduBookActivity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.dalibin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BinbookEduBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_binbook_edu_book)
+    }
+}
